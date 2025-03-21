9afaa349b2900dd40db362781702ce3393b3cd71
ArefinShaon <arefinshaon99@gmail.com>
2025-03-21 14:33:33 +0600

    if elif else statement added

--- second.py
+++ second.py
@@ -1,4 +1,21 @@
 Age =18
 
+#If the statement
 if Age> 15:
+    print("IF the Statement : You are eligible")
+
+# If Else statement
+if Age> 20:
     print("You are eligible")
+else:
+    print("You are not eligible")
+
+# if- elif - else statement
+marks= 89
+
+if marks >=90:
+    print("Great You get good marks")
+elif marks>=85:
+    print("Good You have to work hard")
+else:
+    print("Oops You Failed")
